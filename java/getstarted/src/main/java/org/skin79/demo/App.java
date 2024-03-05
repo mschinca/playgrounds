7a00bf6e81a21d741ea84fb9b31ee9c3e7dfc344
Marco Schincaglia <Marco.Schincaglia@example.com>
2024-03-05 20:16:10 +0000

    Java Hello

---
+++ java/getstarted/src/main/java/org/skin79/demo/App.java
@@ -0,0 +1,13 @@
+package org.skin79.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
